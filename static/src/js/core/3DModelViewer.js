30f56cae7238416f621f44e903e302a89d9dd163
xul <54ulxd@gmail.com>
2023-12-14 20:23:38 -0600

    [ADD] ADDED HDRI TO THE VIEWER

--- static/src/js/core/3DModelViewer.js
+++ static/src/js/core/3DModelViewer.js
@@ -6,38 +6,56 @@ var scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 var rgbeLoader = new RGBELoader();
 
+// Function to remove all objects from the scene
+function removeAllObjects() {
+  while (scene.children.length > 0) {
+    const obj = scene.children[0];
+
+    // Dispose of resources if necessary
+    if (obj instanceof THREE.Mesh) {
+      obj.geometry.dispose();
+      obj.material.dispose();
+    }
+
+    // Remove the object from the scene
+    scene.remove(obj);
+  }
+}
+
 function init3DModelViewer(glbData) {
   // GLTFLoader instance
   var loader = new THREE.GLTFLoader();
   // Function to check if both GLTF and binary data are loaded
   if (glbData) {
     loader.parse(glbData, '', function (gltf) {
-      scene.remove();
+      // renderer.clear();
+      removeAllObjects()
 
       scene.add(gltf.scene);
-      scene.scale.set(3, 3, 3); // Scale by a factor of 2 along all axes
+      scene.scale.set(4, 4, 4); // Scale by a factor of 2 along all axes
 
       // Load the HDRI environment map
-      // rgbeLoader.setDataType(THREE.UnsignedByteType);
-      // rgbeLoader.load('static/autumn_field_4k.hdr', function (texture) {
-      //   var envMap = pmremGenerator.fromEquirectangular(texture).texture;
+      rgbeLoader.setDataType(THREE.FloatType);
+      rgbeLoader.load('static/equirectangular/small_empty_room_3_4k.hdr', function (texture) {
+        const pmremGenerator = new THREE.PMREMGenerator(renderer);
+        var envMap = pmremGenerator.fromEquirectangular(texture).texture;
 
-      //   // Update scene background and ambient light with the HDRI map
-      //   scene.background = envMap;
-      //   scene.environment = envMap;
-      
-      //   // Dispose of the HDR loader to free up memory
-      //   texture.dispose();
-      //   pmremGenerator.dispose();
-      // })
+        // Update scene background and ambient light with the HDRI map
+        scene.background = envMap;
+        scene.environment = envMap;
+        // Dispose of the HDR loader to free up memory
+        texture.dispose();
+        pmremGenerator.dispose();
+      })
+
       // Add ambient light to the scene
-      const ambientLight = new THREE.AmbientLight(0x1E1E1E); // Use a light gray color
-      scene.add(ambientLight);
+      // const ambientLight = new THREE.AmbientLight(0x1E1E1E); // Use a light gray color
+      // scene.add(ambientLight);
 
-      // Create a directional light
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-      directionalLight.position.set(1, 1, 1).normalize(); // Set the direction of the light
-      scene.add(directionalLight);
+      // // Create a directional light
+      // const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+      // directionalLight.position.set(1, 1, 1).normalize(); // Set the direction of the light
+      // scene.add(directionalLight);
 
       // Get the dimensions
       $("#spinner").addClass("hidden");
@@ -51,14 +69,11 @@ function init3DModelViewer(glbData) {
       camera.position.z = 10;
 
       // Initialize renderer
-      renderer.clear();
       renderer.setSize(containerWidth, containerHeight);
       document.getElementById('scene-container').appendChild(renderer.domElement);
 
       const controls = new MapControls(camera, renderer.domElement);
       controls.enableDamping = true;
-      // 
-      var clock = new THREE.Clock();
 
       // Animation loop
       const animate = () => {
